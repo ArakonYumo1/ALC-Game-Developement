b496af9db513a886c01eac30124f853bd2691b6b
Arakon <arakon780@gmail.com>
2017-10-23 12:40:44 -0600

    Added stairs and flashlight

---
+++ test/Assets/Scripts/GhostAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostAI : MonoBehaviour {
+
+	public float moveSpeed;
+	public Transform target;
+	public int damage;
+
+
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "player"){
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+	}
+
+}
