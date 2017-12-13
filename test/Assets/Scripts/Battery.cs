98c9ab09d99d5dcabb65b1899bcb85f63eae445d
Arakon <arakon780@gmail.com>
2017-12-13 13:09:09 -0700

    Finished smooth follow camera

--- test/Assets/Scripts/Battery.cs
+++ test/Assets/Scripts/Battery.cs
@@ -7,8 +7,18 @@ public class Battery : MonoBehaviour {
 
 	public GameObject flashlight;
 
+	GameObject player;
+
+	int checkBat;
+
 	// Use this for initialization
 	void Start () {
+		player = GameObject.FindWithTag("Player");
+
+		flashlight = player;
+
+		//checkBat = flashlight.gameObject.GetComponentInChildren<Flashlight>().currentPower;
+		print("CkBat = "+checkBat);
 	
 	}
 	
@@ -18,7 +28,7 @@ public class Battery : MonoBehaviour {
 	}
 
 	void OnCollisionEnter(Collision other){
-		if(other.gameObject.tag == "Player"){
+		if(other.gameObject.tag == "Player" && flashlight.gameObject.GetComponentInChildren<Flashlight>().currentPower == 0){
 			flashlight.gameObject.GetComponentInChildren<Flashlight>().currentPower = power;
 			Destroy(gameObject);
 			print("You got a battery you n00b!");
